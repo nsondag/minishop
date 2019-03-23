c546918ab820fd7321f6649865e779ff50492bfe
Hubert Vromman <hvromman@w-r2-p5.s19.be>
2019-03-24 00:42:42 +0100

    validate

---
+++ validate.php
@@ -0,0 +1,46 @@
+<html>
+<head>
+	<title>Panier validé</title>
+	<style type="text/css">
+		body {
+			display: table-cell;
+			vertical-align: middle;
+			height: 100vh;
+			width: 100vw;
+		}
+	</style>
+</head>
+<?php
+include 'connect.php';
+include 'header.php';
+?>
+<body>
+<?php
+session_start();
+if (!$_SESSION['login'] || $_SESSION['login'] == '')
+	header('Location: connexion.php');
+else {
+	$conn = connect_db('minishop');
+	$sql = "SELECT user_id FROM user WHERE login='".$_SESSION['login']."'";
+	$user_id = mysqli_fetch_array(mysqli_query($conn, $sql));
+	foreach ($_SESSION['basket'] as $key => $value) {
+		$sql = "INSERT INTO basket VALUES (NULL, ".$user_id['user_id'].", (SELECT prod_id FROM prod WHERE prod_name='".$value['prod_name']."' LIMIT 1), ".$value['nb_prod'].")";
+		echo $sql."<br>";
+		mysqli_query($conn, $sql);
+	}
+}
+?>
+<h1 style="text-align: center;font-size: 10em">
+<?php
+if ($_SESSION['basket'])
+	echo "Validated !";
+else
+	echo "Empty basket !";
+unset($_SESSION['basket']);
+?>
+</h1>
+<p style="text-align: center;font-size: 3em">
+	<a href=".">Back to main page</a>
+</p>
+</body>
+</html>
