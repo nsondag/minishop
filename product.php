bd8ceefe1f015951868fcd73eaf6a7ca74c60f8d
Nadja Sondag <nsondag@w-r2-p3.s19.be>
2019-03-24 15:50:16 +0100

    product

--- product.php
+++ product.php
@@ -1,6 +1,7 @@
 <?php
 include 'util.php';
 $conn = connect_db('minishop');
+session_start();
 $sql = "SELECT * FROM prod WHERE prod_name='".$_GET['prod']."'";
 $res2 = mysqli_query($conn, $sql);
 if (!mysqli_num_rows($res2)) {
@@ -16,6 +17,7 @@ if ($_POST['add']) {
 ?>
 <html>
 <head>
+	<link rel="stylesheet" href="product.css">
 	<title><?php echo $res['prod_name'];?></title>
 </head>
 <?php
@@ -23,10 +25,14 @@ include 'header.php';
 ?>
 <body>
 <?PHP
+echo "<div class=prod>";
 echo "<h1>".strtoupper($_GET['prod'])."</h1>";
 echo "<img class='prod_img' src='" . $res2['image'] . "'>";
+echo "<p class='name'>" . $res2['name']. "</p>\n";
+echo "<p class='price'>" . $res2['price']." € </p>\n";
+echo "</div>";
 ?>
-<form method="post" action="">
+<form class='btn' method="post" action="">
 	<button type="submit" name="add">Ajouter au panier</button>
 </form>
 </body>
